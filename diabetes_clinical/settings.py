de9cfa673d9af5d64306dd3e5f5fcc87277474bf
hardikmittal23 <106722937+hardikmittal23@users.noreply.github.com>
2026-05-08 18:41:10 +0530

    css

--- diabetes_clinical/settings.py
+++ diabetes_clinical/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "clinical_core" / "static",
 ]
@@ -129,6 +129,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
